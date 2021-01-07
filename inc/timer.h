e6cb6cb804ae42ba086ba4f02d8686e491ff96fa
hotarou1509 <thaiviet271@gmail.com>
2021-01-07 22:11:36 +0700

    add project info

--- inc/timer.h
+++ inc/timer.h
@@ -1,3 +1,18 @@
+/**************************************************************************
+*
+*	Purpose	:	Managing project's header file
+*	Author	:	THAI VIET LE
+*	Updated	:	1/7/2021
+*	
+***************************************************************************
+*	Copyright (C) 2021 by THAI VIET LE
+***************************************************************************
+/**************************** STEPS TO FOLLOW *****************************
+* 1.	Enable Timer Clock
+* 2.	Set the prescalar and the ARR
+* 3.	Enable the Timer and wait for the Update Flag to Set
+**************************************************************************/
+
 void Timer_Init(void){
 	// 1.	Enable Timer Clock
 	RCC->APB1ENR |= (1<<4);	// Enable the timer6 clock
